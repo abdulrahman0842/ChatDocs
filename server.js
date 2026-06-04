303b86c163238882235c6317322f72e727c6271f
Abdul Rahman <abdulrahmanmlg744@gmail.com>
2026-06-04 19:03:10 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express"
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    res.json({ msg: "Hello" })
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+
+})
